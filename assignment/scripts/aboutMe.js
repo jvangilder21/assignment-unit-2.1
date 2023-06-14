a89eb82c21748ed7f745beed6daa3425b70b5999
Jenny Van Gilder <jkosty21@gmail.com>
2023-06-13 20:50:39 -0500

    finsihed Q16 & Q18

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,8 +1,10 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Jenny';
+console.log(firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let LastName = 'Van Gilder';
+console.log(LastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName + " " + LastName;
@@ -10,6 +12,7 @@ let fullName = firstName + " " + LastName;
 console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 21;
+console.log('My fav number is ' + luckyNumber);
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
@@ -18,12 +21,16 @@ console.log('My name is ' + fullName + ', and I think ' + luckyNumber + ' is a w
 let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'Buttered Noodles!'
+console.log(food + ' ' + 'Are my favorite!');
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 2;
+console.log('We have ' + pets + ' pets.');
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets = 3;
+console.log('Our friends have ' + friendsPets +'!');
 // 11 - Add two pets to your `pets` variable
-pets = pets +2;
+pets = pets + 2;
+console.log('We now have ' + pets + ' pets.');
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 const allowedPets = 3;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
@@ -33,7 +40,6 @@ if (adventurous == true) {
 }else{
     console.log("How about we stay home?");
 }
-
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 if (luckyNumber == 2 && adventurous == true) {
@@ -59,6 +65,18 @@ if (pets < allowedPets) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+let mostPets;
+if (pets > friendsPets){
+    mostPets = pets;
+   console.log('pets is greater than friendsPets with total', mostPets);
+}else if (pets < friendsPets){
+    mostPets = friendsPets;
+   console.log('friendsPets is greater than pets with total', mostPets);
+}else if (pets = friendsPets){
+    mostPets = friendsPets && pets;
+   console.log('friendsPets has the same total pets with total', mostPets);
+}
+
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
@@ -66,7 +84,25 @@ if (pets < allowedPets) {
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+switch (luckyNumber) {
+    case 1:
+        console.log("First is the worst");
+        break;
+    case 2:
+      console.log("Second is the best");
+      break;
+    case 3:
+        console.log("Third is the one with the polka dot dress");
+        break;
+     default:
+        console.log("Luck is what happens when preparation meets opportunity");
+        break;
+  }
+
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
+//conidition ? condition is true output : condition is false output
+adventurous === true ? console.log("Adventures are great!") : console.log("How about we stay home?")
 
+  
